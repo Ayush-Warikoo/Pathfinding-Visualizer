12b5b05a2eb562239d7e21835f05cf0d8fd8aa9e
Ayush Warikoo <ayush.warikoo77@gmail.com>
2020-12-30 22:36:38 -0500

    Initial Commit

---
+++ src/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header';
+
+function MainPage() {
+    return (
+        <div className="MainPage">
+            <Header> </Header>
+            <h1> Main Page </h1>
+        </div>
+    )
+}
+
+export default MainPage
