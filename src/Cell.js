1aa13a73005a70f1ba690bbc536a1bd4091f69fe
Ayush Warikoo <ayush.warikoo77@gmail.com>
2021-01-01 16:09:59 -0500

    Added cell prop

--- src/Cell.js
+++ src/Cell.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './Cell.css';
 
+function Cell({ cell }) {
 
-function Cell() {
+    useEffect(() => {
+    }, [cell])
+
+    const isVisitedClass = () =>
+    {
+        if(cell.visited)
+        {
+            return "Visited";
+        }
+        return "NotVisited";
+    }
+
+    const onClickFunc = () =>
+    {
+        //console.log("A");
+        //console.log(cell.row, cell.col);
+    }
+
+    const onMouseDownFunc = () =>
+    {
+        //console.log("B");
+    }
+
     return (
-        <div className="Cell">
+        <div className={"Cell " + "Cell__" + cell.state + " Cell__" + isVisitedClass()} onClick={onClickFunc()} onMouseDown={onMouseDownFunc()}>
         </div>
     )
 }
 
 export default Cell
+
+
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,31 +1,56 @@
 import React, {useState, useEffect } from 'react';
 import Header from './Header';
-import Cell from './Cell';
+import Cell  from './Cell';
 import './MainPage.css';
 
 function MainPage() {
+
+    const STARTING_CELL = [0, 0];
+    const FINISHING_CELL = [19, 49];
+
     const [grid, setGrid] = useState([]);
 
     useEffect(() => {
-        let cells = [];
+        let grid = [];
         for(let r = 0; r < 20; r++)
         {
             let currRow = [];
             for(let c = 0; c < 50; c++)
             {
-                currRow.push([]);
+                //State can be None, Start, Finish or Wall
+                let cell = {
+                    row: r,
+                    col: c,
+                    visited: false, 
+                    state: "None"
+                }
+                if(r == STARTING_CELL[0] && c == STARTING_CELL[1])
+                {
+                    cell["state"] = "Start";
+                }
+                else if(r == FINISHING_CELL[0] && c == FINISHING_CELL[1])
+                {
+                    cell["state"] = "Finish";
+                }
+                currRow.push(cell);
             }
-            cells.push(currRow);
+            grid.push(currRow);
         }
-        setGrid(cells);
+        setGrid(grid);
     }, [])
+
+    
 
     return (
         <div className="MainPage">
-            <Header> </Header>
+            <Header/>
             <div className="Grid">
-                { grid.map((row, indr) => {
-                    return (<div className="Grid__row"> {row.map((node, indn) => <Cell> </Cell>)} </div>)
+                {grid.map((row, indexR) => {
+                    return (
+                        <div className="Grid__row"> 
+                            {row.map((col, indexC) => <Cell cell = {grid[indexR][indexC]}  />)} 
+                        </div>
+                    )
                 })}
             </div>
             
